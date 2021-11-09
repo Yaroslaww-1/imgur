9877686c9dc952e3b00d4e11e626b40bdc20eaef
Yaroslaww-1 <yaroslaww1@gmail.com>
2021-11-09 23:08:09 +0200

    backend[gateway]: added initial setup

---
+++ backend/GatewayApi/Options/ServicesOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediaLakeGatewayApi.Options
+{
+    public class ServicesOptions
+    {
+        public const string Position = "Services";
+
+        public Dictionary<string, string> Hosts { get; set; }
+    }
+}
---
+++ backend/GatewayApi/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using IdentityServer4.AccessTokenValidation;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using MediaLakeGatewayApi.Options;
+using System;
+
+namespace MediaLakeGatewayApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json")
+                .AddJsonFile("ocelot.json")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var authenticationProviderKey = "IdentityApiKey";
+
+            services.AddCors();
+
+            services.AddOcelot(Configuration);
+
+            services.AddAuthentication()
+                .AddIdentityServerAuthentication(authenticationProviderKey, options =>
+                {
+                    options.Authority = Configuration.GetSection(ServicesOptions.Position).Get<ServicesOptions>().Hosts["Users"];
+                    options.SupportedTokens = SupportedTokens.Jwt;
+                    options.ApiSecret = "secret";
+                    options.RequireHttpsMetadata = false;
+                });
+
+            var servicesOptions = Configuration
+                .GetSection(ServicesOptions.Position)
+                .Get<ServicesOptions>();
+            services.ConfigureDownstreamHostAndPortsPlaceholders(servicesOptions.Hosts);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
