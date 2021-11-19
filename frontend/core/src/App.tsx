61625079f2802e99a8dbe621d086ef88023ebae4
fireman9 <nazariystr@gmail.com>
2021-11-20 00:43:31 +0200

    frontend[core]: added routing and post creation to it

--- frontend/core/src/App.tsx
+++ frontend/core/src/App.tsx
@@ -1,3 +1,14 @@
+import { Route, Switch } from "react-router-dom";
+
+import { CreatePost } from "pages/create-post";
+import { AppRoute } from "@common/enums/app-route.enum";
+
 export const App = () => {
-  return <>Cloynada</>;
+  return (
+    <>
+      <Switch>
+        <Route path={AppRoute.CREATE_POST} component={CreatePost} />
+      </Switch>
+    </>
+  );
 };
--- frontend/core/src/index.tsx
+++ frontend/core/src/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 import { App } from "./App";
 
@@ -7,8 +9,10 @@ import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={createBrowserHistory()}>
+      <App />
+    </Router>
   </React.StrictMode>,
   // eslint-disable-next-line comma-dangle
-  document.getElementById("root")
+  document.getElementById("root"),
 );
