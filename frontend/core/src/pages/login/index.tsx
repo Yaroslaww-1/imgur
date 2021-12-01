bc82b2e841948e038efdaf0c1836ca64268af2af
fireman9 <nazariystr@gmail.com>
2021-12-01 11:57:47 +0200

    frontend[core]: added store and extended functionality in login component

--- frontend/core/src/pages/login/index.tsx
+++ frontend/core/src/pages/login/index.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+
+import { Context } from "index";
 
 import { Page } from "@components/page";
 import { Input } from "@components/input";
 import { SimpleButton } from "@components/buttons/simple-button";
 
 import styles from "./styles.module.scss";
+import { observer } from "mobx-react-lite";
 
-export const Login: React.FC = () => {
+export const Login: React.FC = observer(() => {
+  const { store } = useContext(Context);
   const form = React.createRef<HTMLFormElement>();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    store.login(email, password);
   }
 
   return (
@@ -20,8 +27,22 @@ export const Login: React.FC = () => {
       </div>
       <form className={styles.formContent} onSubmit={onSubmit} ref={form}>
         <div className={styles.inputs}>
-          <Input text={"Email"} type={"email"} required />
-          <Input text={"Password"} type={"password"} required />
+          <Input
+            text={"Email"}
+            type={"email"}
+            required
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <Input
+            text={"Password"}
+            type={"password"}
+            required
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setPassword(e.target.value);
+            }}
+          />
         </div>
         <SimpleButton text={"Sign in"} size={"medium"} type={"submit"} />
       </form>
@@ -29,6 +50,14 @@ export const Login: React.FC = () => {
         <h3>Not signed up?</h3>
         <SimpleButton text={"Sign up"} size={"small"} />
       </div>
+
+      <SimpleButton
+        text={"Log out"}
+        size={"small"}
+        onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
+          store.logout();
+        }}
+      />
     </Page>
   );
-};
+});
