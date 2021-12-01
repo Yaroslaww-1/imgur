44e4b703ab7d4b39b70b3e782ce60afc686a004d
fireman9 <nazariystr@gmail.com>
2021-12-01 16:05:54 +0200

    frontend[core]: added post creating with empty name

--- frontend/core/src/api/services/posts.service.ts
+++ frontend/core/src/api/services/posts.service.ts
@@ -1,15 +1,15 @@
 import api from "../api.helper";
 
-const endpoint = "";
+const endpoint = "/api/core/posts";
 
 export class PostsService {
   static async createPost(
     content: string,
     image: File | string,
   ): Promise<void> {
-    const formData = new FormData();
-    formData.append("content", content);
-    formData.append("image", image);
-    api.post(endpoint, formData);
+    // const formData = new FormData();
+    // formData.append("content", content);
+    // formData.append("image", image);
+    api.post(endpoint, { name: "empty", content });
   }
 }
