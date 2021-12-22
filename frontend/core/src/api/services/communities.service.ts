d4c2f1bba41e6a8017ddfe9fd8a55adde5bd3813
fireman9 <nazariystr@gmail.com>
2021-12-22 03:02:56 +0200

    frontend[core]: added request for all and only user communities

--- frontend/core/src/api/services/communities.service.ts
+++ frontend/core/src/api/services/communities.service.ts
@@ -2,10 +2,14 @@ import api from "../api.helper";
 
 import { ICommunity } from "@models/community.model";
 
-const endpoint = "/api/core/communities/authenticatedUser";
+const endpoint = "/api/core/communities";
 
 export class CommunitiesService {
   static async getUserCommunities(): Promise<ICommunity[]> {
+    return api.get(endpoint + "/authenticatedUser");
+  }
+
+  static async getAllCommunities(): Promise<ICommunity[]> {
     return api.get(endpoint);
   }
 }
